a896fb951f477571ea6be7d3f3926f9a1bef1f2e
Ethan Quarry <dev.ethanquarry@gmail.com>
2024-03-31 20:06:00 +0100

    Fix: Creating/Updating flashcards completely works.

--- src/routes/decks.route.ts
+++ src/routes/decks.route.ts
@@ -1,7 +1,6 @@
 // routes/deckRouter.ts
 import express from 'express';
 import db from '@/config/db';
-import { randomUUID } from 'crypto';
 import { Flashcard, DatabaseFlashcard } from '@/interfaces/routes.interface';
 
 const router = express.Router();
@@ -55,39 +54,31 @@ router.post('/decks/get', async (req, res) => {
 router.post('/decks/update', async (req, res) => {
   try {
      const { id, flashcards } = req.body;
-    if (id && flashcards) {
-      const upsertFlashCards = await Promise.all(flashcards.map(flashcard => {
-          if (flashcard.id) {
-            return db.flashCard.upsert({
-              where: { id: flashcard.id }, 
-              update: {
-                term: flashcard.term,
-                definition: flashcard.definition,
-                deckId: id,
-              },
-              create: {
-                term: flashcard.term,
-                definition: flashcard.definition,
-                deckId: id,
-              },
-            })
-          } else {
-            return db.flashCard.create({
-              data: {
-                term: flashcard.term,
-                definition: flashcard.definition,
-                deckId: id,
-              }
-            })
-          
-          }
-      }
-      ));
-
-     res.status(200).json(upsertFlashCards);
-    }
-
-     
+    
+     if (id && flashcards) {
+       const upsertFlashCards = await db.$transaction(
+         flashcards.map((flashcard: DatabaseFlashcard) => {
+           return db.flashCard.upsert({
+             where: { id: flashcard.id }, 
+             update: {
+               term: flashcard.term,
+               definition: flashcard.definition,
+               deckId: id,
+             },
+             create: {
+               id: flashcard.id,
+               term: flashcard.term,
+               definition: flashcard.definition,
+               deckId: id,
+             },
+           });
+         })
+       );
+ 
+       res.status(200).json(upsertFlashCards);
+     } else {
+       res.status(400).json({ error: 'Invalid request body.' });
+     }
   } catch (error) {
      console.error(error);
      res.status(500).json({ error: 'An error occurred while updating the flashcards.' });
